f935995f45bb2040656c98378b5cb131c8921f26
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-05-12 23:10:25 +0100

    Code tidy with Pandas

--- main.py
+++ main.py
@@ -36,30 +36,32 @@ import pandas as pd
 # plt.axis('equal')
 # plt.show()
 
-data = [{
-    'name': 'Nick',
-    'jan_ir': 124,
-    'feb_ir': 100,
-    'march_ir': 165
-    },
-    {
-    'name': 'Panda',
-    'jan_ir': 112,
-    'feb_ir': 143,
-    'march_ir': 3
-    },
-    {
-        'name': 'Panda',
-        'jan_ir': 112,
-        'feb_ir': 143,
-        'march_ir': 3}
-]
-
-raw_data = {'names': ['Nick', 'Pandas', 'S', 'Ari', 'Valos'],
-            'jan_ir': [143, 122, 101, 106, 365],
-            'feb_ir': [122, 132, 144, 98, 62],
-            'march_ir': [65, 88, 12, 32, 65]}
-
+## Before panda
+#
+# data = [{
+#     'name': 'Nick',
+#     'jan_ir': 124,
+#     'feb_ir': 100,
+#     'march_ir': 165
+#     },
+#     {
+#     'name': 'Panda',
+#     'jan_ir': 112,
+#     'feb_ir': 143,
+#     'march_ir': 3
+#     },
+#     {
+#         'name': 'Panda',
+#         'jan_ir': 112,
+#         'feb_ir': 143,
+#         'march_ir': 3}
+# ]
+#
+# raw_data = {'names': ['Nick', 'Pandas', 'S', 'Ari', 'Valos'],
+#             'jan_ir': [143, 122, 101, 106, 365],
+#             'feb_ir': [122, 132, 144, 98, 62],
+#             'march_ir': [65, 88, 12, 32, 65]}
+#
 
 
 df = pd.DataFrame(raw_data, columns=['names', 'jan_ir', 'feb_ir', 'march_ir'])
