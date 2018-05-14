b5f4fe3744dcb3611a39d46ce53c1562afc3f58a
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-05-14 15:37:39 +0100

    Bar charts with Panda

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-
 import matplotlib.pyplot as plt
 import pandas as pd
+
+import numpy as np
+
 
 # years = [1950, 1955, 1960, 1965, 1970, 1975, 1980, 1985, 1990, 1995,
 #          2000, 2005, 2010, 2015]
@@ -57,26 +59,44 @@ import pandas as pd
 #         'march_ir': 3}
 # ]
 #
-raw_data = {'names': ['Nick', 'Pandas', 'S', 'Ari', 'Valos'],
-            'jan_ir': [143, 122, 101, 106, 365],
-            'feb_ir': [122, 132, 144, 98, 62],
-            'march_ir': [65, 88, 12, 32, 65]}
 
 
 
-df = pd.DataFrame(raw_data, columns=['names', 'jan_ir', 'feb_ir', 'march_ir'])
+# raw_data = {'names': ['Nick', 'Pandas', 'S', 'Ari', 'Valos'],
+#             'jan_ir': [143, 122, 101, 106, 365],
+#             'feb_ir': [122, 132, 144, 98, 62],
+#             'march_ir': [65, 88, 12, 32, 65]}
+#
+#
+#
+# df = pd.DataFrame(raw_data, columns=['names', 'jan_ir', 'feb_ir', 'march_ir'])
+#
+# df['total_ir'] = df['jan_ir'] + df['feb_ir'] + df ['march_ir']
+#
+# print(df)
+#
+# color = [(1, .4, .4), (1,.6,1), (.5, .3, 1),( .3, 1, .5), (.7, .7, .2)]
+#
+# plt.pie(df['total_ir'],
+#         labels=df['names'],
+#         colors=color,
+#         autopct='%1.1f%%')
+#
+# plt.axis('equal')
+#
+# plt.show()
 
-df['total_ir'] = df['jan_ir'] + df['feb_ir'] + df ['march_ir']
+col_count = 3
 
-print(df)
+korea_scores = (554, 536, 538)
+canada_scores = (518, 523, 525)
+china_scores = (613, 570, 580)
+ireland_scores = (501, 523, 522)
+france_scores = (495, 505, 499)
 
-color = [(1, .4, .4), (1,.6,1), (.5, .3, 1),( .3, 1, .5), (.7, .7, .2)]
+index = np.arange(col_count)
 
-plt.pie(df['total_ir'],
-        labels=df['names'],
-        colors=color,
-        autopct='%1.1f%%')
-
-plt.axis('equal')
+k1 = plt.bar(index, korea_scores, .5)
+plt.grid(True)
 
 plt.show()
