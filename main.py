a654e9c07a5f083d75000793d3ca2e7712ee6cf0
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-05-15 15:35:47 +0100

    Bar chart with more data & legends

--- main.py
+++ main.py
@@ -87,6 +87,7 @@ import numpy as np
 # plt.show()
 
 col_count = 3
+bar_width = .1
 
 korea_scores = (554, 536, 538)
 canada_scores = (518, 523, 525)
@@ -96,7 +97,18 @@ france_scores = (495, 505, 499)
 
 index = np.arange(col_count)
 
-k1 = plt.bar(index, korea_scores, .5)
+k1 = plt.bar(index, korea_scores, bar_width, alpha=.4,label="Korea")
+c1 = plt.bar(index + 0.1, canada_scores, bar_width,alpha=.4,label="Canada")
+ch1 = plt.bar(index + 0.2, china_scores, bar_width,alpha=.4,label="China")
+f1 = plt.bar(index + 0.3, france_scores, bar_width,alpha=.4,label="France")
+irl = plt.bar(index + 0.4, ireland_scores, bar_width,alpha=.4,label="Eire")
+
+plt.ylabel("Mean score in PISA 2012")
+plt.xlabel("Subjects")
+plt.title("Test Scores by Country")
+
+plt.xticks(index + .3 / 2, ("Mathematics", "Reading", "Science"))
+plt.legend()
 plt.grid(True)
 
 plt.show()
