fe60ea60a00d312c9ec16f7c94c83b1d50d663a4
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-05-16 16:04:30 +0100

    Specific numbers for each chart

--- main.py
+++ main.py
@@ -98,10 +98,22 @@ france_scores = (495, 505, 499)
 index = np.arange(col_count)
 
 k1 = plt.bar(index, korea_scores, bar_width, alpha=.4,label="Korea")
-c1 = plt.bar(index + 0.1, canada_scores, bar_width,alpha=.4,label="Canada")
-ch1 = plt.bar(index + 0.2, china_scores, bar_width,alpha=.4,label="China")
-f1 = plt.bar(index + 0.3, france_scores, bar_width,alpha=.4,label="France")
-irl = plt.bar(index + 0.4, ireland_scores, bar_width,alpha=.4,label="Eire")
+c1 = plt.bar(index + 0.2, canada_scores, bar_width,alpha=.4,label="Canada")
+ch1 = plt.bar(index + 0.4, china_scores, bar_width,alpha=.3,label="China")
+f1 = plt.bar(index + 0.6, france_scores, bar_width,alpha=.4,label="France")
+irl = plt.bar(index + 0.8, ireland_scores, bar_width,alpha=.4,label="Eire")
+
+def CreateLabels(data):
+    for item in data:
+        height = item.get_height()
+        plt.text(item.get_x() + item.get_width() / 2., height*1.05,
+                 '%d' % int(height), ha='center', va='bottom')
+
+CreateLabels(k1)
+CreateLabels(c1)
+CreateLabels(ch1)
+CreateLabels(f1)
+CreateLabels(irl)
 
 plt.ylabel("Mean score in PISA 2012")
 plt.xlabel("Subjects")
@@ -110,5 +122,8 @@ plt.title("Test Scores by Country")
 plt.xticks(index + .3 / 2, ("Mathematics", "Reading", "Science"))
 plt.legend()
 plt.grid(True)
+
+for item in k1:
+    print(item.get_height(), item.get_width(), item.get_x())
 
 plt.show()
